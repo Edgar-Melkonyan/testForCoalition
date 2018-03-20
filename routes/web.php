14881a4d3e3159e26fdfd7d416047998fbbc071b
Edgar Melkonyan <edgar365224@gmail.com>
2018-03-20 21:05:52 +0400

    Commited the app

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','ProductController@index');
+
+Route::post('/store_product' , 'ProductController@storeProduct');
+
+Route::get('/get_json' , 'ProductController@getJsonData');
